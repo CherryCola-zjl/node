1767c035e03968b8637a6203b6f2915afa2195a0
z <502242791@qq.com>
2023-10-26 15:50:37 +0800

    init

--- test.js
+++ test.js
@@ -1,26 +1,32 @@
-var mysql = require('mysql2');
+var mysql = require("mysql2");
 var connection = mysql.createConnection({
-  host     : 'localhost',
+  host: "localhost",
   port: 55007,
-  user     : 'root',
-  password : 'mysqlpw'
+  user: "root",
+  password: "mysqlpw",
 });
- 
+
 connection.connect();
- 
-connection.query('CREATE DATABASE IF NOT EXISTS zang DEFAULT CHARSET utf8mb4 COLLATE utf8mb4_general_ci;', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results);
-});
 
-connection.query('use zang;')
+connection.query(
+  "CREATE DATABASE IF NOT EXISTS zang DEFAULT CHARSET utf8mb4 COLLATE utf8mb4_general_ci;",
+  function (error, results, fields) {
+    if (error) throw error;
+    console.log("The solution is: ", results);
+  }
+);
 
-connection.query(`CREATE TABLE IF NOT EXISTS user(
+connection.query("use zang;");
+
+connection.query(
+  `CREATE TABLE IF NOT EXISTS user(
     name text,
     age int
-)`, function (error, results, fields) {
+)`,
+  function (error, results, fields) {
     if (error) throw error;
-    console.log('The solution is: ', results);
-});
+    console.log("The solution is: ", results);
+  }
+);
 
 connection.end();
