cf3ce0143f042be23b5dae81f1b2d4786eb353a4
z <502242791@qq.com>
2023-10-27 19:04:19 +0800

    mysql

--- test.js
+++ test.js
@@ -1,23 +1,22 @@
 var mysql = require("mysql2");
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 55007,
+  port: 55004,
   user: "root",
   password: "mysqlpw",
 });
 
 connection.connect();
-
+// 创建数据库
 connection.query(
-  "CREATE DATABASE IF NOT EXISTS zang DEFAULT CHARSET utf8mb4 COLLATE utf8mb4_general_ci;",
+  "CREATE DATABASE IF NOT EXISTS user DEFAULT CHARSET utf8mb4 COLLATE utf8mb4_general_ci;",
   function (error, results, fields) {
     if (error) throw error;
     console.log("The solution is: ", results);
   }
 );
 
-connection.query("use zang;");
-
+connection.query("use user;");
 connection.query(
   `CREATE TABLE IF NOT EXISTS user(
     name text,
